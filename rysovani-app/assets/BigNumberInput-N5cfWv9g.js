66157a44210f145bd80f95e15671b2a7a4751b01
vividbooks <vitek@vividbooks.com>
2026-02-24 17:30:52 +0100

    Updates

--- rysovani-app/assets/BigNumberInput-N5cfWv9g.js
+++ rysovani-app/assets/BigNumberInput-N5cfWv9g.js
@@ -1,4 +1,4 @@
-import{c as N,r as y,j as a,C as S}from"./index-CAZlzlD0.js";/**
+import{c as N,r as y,j as a,C as S}from"./index-DjzhsdjJ.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- rysovani-app/assets/slider-D-N7mpZN.js
+++ rysovani-app/assets/slider-D-N7mpZN.js
@@ -1,4 +1,4 @@
-import{c as G,r as c,j as C,f as ft,R as H}from"./index-CAZlzlD0.js";/**
+import{c as G,r as c,j as C,f as ft,R as H}from"./index-DjzhsdjJ.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
