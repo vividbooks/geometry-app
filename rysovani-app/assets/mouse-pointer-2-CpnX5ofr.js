16a590f0657cca817ac8b1fadfc5c7304e8011fd
vividbooks <vitek@vividbooks.com>
2026-09-23 17:32:00 +0200

    Updates

--- rysovani-app/assets/mouse-pointer-2-CpnX5ofr.js
+++ rysovani-app/assets/mouse-pointer-2-CpnX5ofr.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Cxo5HLwv.js";/**
+import{c as a}from"./index-ZrgxTs2A.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
